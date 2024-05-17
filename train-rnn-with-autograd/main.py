1116d21bb8d43d4cc22ce86bfa78940bb920a620
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-17 22:09:19 +0300

    Fix the code.

--- train-rnn-with-autograd/main.py
+++ train-rnn-with-autograd/main.py
@@ -5,8 +5,10 @@ import numpy as np
 import torch
 from torch.nn.functional import softmax
 
+os.environ["KERAS_BACKEND"] = "torch"
+
 SEQ_LENGTH = 100
-BATCH_SIZE = 512
+BATCH_SIZE = 64
 
 
 def random_inputs_and_targets(songs, seq_length, batch_size):
@@ -18,15 +20,14 @@ def random_inputs_and_targets(songs, seq_length, batch_size):
 
 def generate_songs(model, char_to_index, index_to_char, start_string, generation_length=1000):
     input_eval = [char_to_index[s] for s in start_string]
-    input_eval = torch.unsqueeze(torch.tensor(input_eval), 0)
 
     text_generated = []
 
     for i in range(generation_length):
-        predictions = torch.squeeze(model(input_eval), 0)
-        predicted_index = torch.multinomial(softmax(predictions, dim=0), 1, replacement=True)[-1, 0]
-        input_eval = torch.unsqueeze(torch.unsqueeze(predicted_index, 0), 0)
-        text_generated.append(index_to_char[predicted_index.item()])
+        predictions = model(torch.unsqueeze(torch.tensor(input_eval), 0))
+        predicted_index = torch.multinomial(torch.squeeze(predictions).exp(), 1).item()
+        input_eval = [predicted_index]
+        text_generated.append(index_to_char[predicted_index])
 
     return start_string + ''.join(text_generated)
 
@@ -49,25 +50,26 @@ if __name__ == '__main__':
     index_to_char, char_to_index = np.array(vocabulary), {u: i for i, u in enumerate(vocabulary)}
     vectorized_songs = np.array([char_to_index[character] for character in songs])
 
-    model = build_model(BATCH_SIZE, False)
-    model.summary()
-
-    loss_function = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=5e-3)
-    for i in range(1000):
-        inputs, targets = random_inputs_and_targets(vectorized_songs, seq_length=SEQ_LENGTH, batch_size=BATCH_SIZE)
-
-        predictions = model(inputs)
-        loss = loss_function(predictions.permute((0, 2, 1)), torch.from_numpy(targets).long().cuda())
-
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        if i % 10 == 0:
-            print("Loss at iteration", i, "is", loss.item())
-
-    torch.save(model.state_dict(), os.path.join(cwd, "model.pt"))
-    print("The model has been saved")
+    # model = build_model(BATCH_SIZE, False)
+    # model.summary()
+    #
+    # loss_function = torch.nn.CrossEntropyLoss()
+    # optimizer = torch.optim.Adam(model.parameters(), lr=5e-3)
+    # for i in range(500):
+    #     inputs, targets = random_inputs_and_targets(vectorized_songs, seq_length=SEQ_LENGTH, batch_size=BATCH_SIZE)
+    #
+    #     predictions = model(inputs)
+    #     loss = loss_function(predictions.permute(0, 2, 1), torch.from_numpy(targets).long().cuda())
+    #
+    #     model.zero_grad()
+    #     loss.backward()
+    #
+    #     optimizer.step()
+    #     if i % 10 == 0:
+    #         print("Loss at iteration", i, "is", loss.item())
+    #
+    # torch.save(model.state_dict(), os.path.join(cwd, "model.pt"))
+    # print("The model has been saved")
 
     trained_model = build_model(1, True)
     trained_model.load_state_dict(torch.load(os.path.join(cwd, "model.pt")))
