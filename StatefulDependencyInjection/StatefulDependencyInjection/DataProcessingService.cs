e990466136dae67f6b41736eedab40c93dfbf22a
yaskovdev <yaskovdev@gmail.com>
2024-10-27 19:05:30 +0200

    Add comments describing the problem.

--- StatefulDependencyInjection/StatefulDependencyInjection/DataProcessingService.cs
+++ StatefulDependencyInjection/StatefulDependencyInjection/DataProcessingService.cs
@@ -8,7 +8,17 @@ public class DataProcessingService(ILogger<DataProcessingService> logger) : IDat
 
     public void StartProcessing(int socketId)
     {
-        _sessions.GetOrAdd(socketId, new Session(socketId, new DataProcessor(logger)));
+        _sessions.GetOrAdd(socketId, new Session(socketId, CreateDataProcessor()));
+    }
+
+    private DataProcessor CreateDataProcessor()
+    {
+        // What if the Decoder and Encoder have dependencies themselves?
+        // What if the dependencies in turn need their own dependencies?
+        // What if one of the leaves of this potentially big dependency tree needs a dependency from the dependency
+        // container, say, the logger? Then the logger becomes a pass-through dependency.
+        return new DataProcessor(
+            logger /*, new Decoder(new DecoderDependency(new StatisticsCollector(), logger)), new Encoder()*/);
     }
 
     public void StopProcessing(int socketId)
