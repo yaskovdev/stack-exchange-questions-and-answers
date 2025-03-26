b4a06017e89756aa06bc424a1ddfdf2d26132864
Sergey Yaskov <yaskovdev@gmail.com>
2025-03-26 11:10:38 +0200

    Check if Stream.FlushAsync creates a new block (it does).

--- AzureStorageBlobsBlockSize/AzureStorageBlobsBlockSize/Program.cs
+++ AzureStorageBlobsBlockSize/AzureStorageBlobsBlockSize/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace AzureStorageBlobsBlockSize;
 
-Console.WriteLine("Hello, World!");
+using System.Collections.Immutable;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+internal static class Program
+{
+    private const long WriteBufferSizeInBytes = 100 * 1024 * 1024;
+
+    public static async Task Main(string[] args)
+    {
+        var blobContainerName = $"container-{Guid.NewGuid()}";
+        var blobServiceClient = new BlobServiceClient("UseDevelopmentStorage=true");
+        var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+        var containerCreateResponse = await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.None, ImmutableDictionary<string, string>.Empty, CancellationToken.None);
+        if (containerCreateResponse == null)
+        {
+            throw new Exception($"Unable to create blob container: container {blobContainerName} already exists");
+        }
+        var appendBlobClient = blobContainerClient.GetAppendBlobClient($"blob-{Guid.NewGuid()}");
+        var stream = await appendBlobClient.OpenWriteAsync(false, new AppendBlobOpenWriteOptions { BufferSize = WriteBufferSizeInBytes }, CancellationToken.None);
+
+        // Write 8000 bytes to the append blob, expect it to be written in one block
+        for (var i = 0; i < 1000; i++)
+        {
+            await stream.WriteAsync(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }, CancellationToken.None);
+            await stream.FlushAsync();
+        }
+    }
+}
