9a80822aed692c87bf2098bf73c75722ed36d16a
Sergey Yaskov <yaskovdev@gmail.com>
2025-03-26 11:16:43 +0200

    Add a comment.

--- AzureStorageBlobsBlockSize/AzureStorageBlobsBlockSize/Program.cs
+++ AzureStorageBlobsBlockSize/AzureStorageBlobsBlockSize/Program.cs
@@ -22,7 +22,8 @@ internal static class Program
         var appendBlobClient = blobContainerClient.GetAppendBlobClient($"blob-{Guid.NewGuid()}");
         var stream = await appendBlobClient.OpenWriteAsync(false, new AppendBlobOpenWriteOptions { BufferSize = WriteBufferSizeInBytes }, CancellationToken.None);
 
-        // Write 8000 bytes to the append blob, expect it to be written in one block
+        // Write 8000 bytes to the append blob, expect it to be written in one block.
+        // Actually, it will be written in 1000 blocks of 8 bytes each, see the ContentLength and the CommittedBlockCount of the resulting blob.
         for (var i = 0; i < 1000; i++)
         {
             await stream.WriteAsync(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }, CancellationToken.None);
