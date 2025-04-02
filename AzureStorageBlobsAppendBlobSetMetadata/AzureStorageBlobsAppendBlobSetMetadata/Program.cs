67b341d986a49290e05192f2f24959b89b41e222
Sergey Yaskov <yaskovdev@gmail.com>
2025-04-03 00:28:40 +0300

    Implement the repro.

--- AzureStorageBlobsAppendBlobSetMetadata/AzureStorageBlobsAppendBlobSetMetadata/Program.cs
+++ AzureStorageBlobsAppendBlobSetMetadata/AzureStorageBlobsAppendBlobSetMetadata/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace AzureStorageBlobsAppendBlobSetMetadata;
 
-Console.WriteLine("Hello, World!");
+using System.Collections.Immutable;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var blobContainerName = $"container-{Guid.NewGuid()}";
+        var blobServiceClient = new BlobServiceClient("UseDevelopmentStorage=true");
+        var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+        var containerCreateResponse = await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.None, ImmutableDictionary<string, string>.Empty, CancellationToken.None);
+        if (containerCreateResponse == null)
+        {
+            throw new Exception($"Unable to create blob container: container {blobContainerName} already exists");
+        }
+
+        var blob = blobContainerClient.GetAppendBlobClient($"blob-{Guid.NewGuid()}");
+        var stream = await blob.OpenWriteAsync(false, new AppendBlobOpenWriteOptions { BufferSize = 4 }, CancellationToken.None);
+
+        for (var i = 0; i < 1000; i++)
+        {
+            var payload = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine($"Appending {payload.Length} bytes to blob");
+            await stream.WriteAsync(payload, CancellationToken.None);
+            Console.WriteLine("Setting metadata for blob");
+            await blob.SetMetadataAsync(ImmutableDictionary<string, string>.Empty.Add("key", "value"));
+            await Task.Delay(1000);
+        }
+    }
+}
